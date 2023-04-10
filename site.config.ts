c23bdd1bd9b988df8f3e8236c36c05be162a0c67
miu321 <63976980+miu321@users.noreply.github.com>
2023-04-10 13:14:20 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'fb9c26cb1bec43fcac28c8ea1721f326',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '个人Notion博客',
+  domain: 'https://www.notion.so/fb9c26cb1bec43fcac28c8ea1721f326',
+  author: 'Brian Miu',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
